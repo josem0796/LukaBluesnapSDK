c39afae56ae970519878f1cc64224a8c4c633290
Carlos Villamizar <cvillamizar@me.com>
2023-10-31 14:57:13 -0400

    Initial Commit

---
+++ LukaBluesnapSDK/LukaBluesnapSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  LukaBluesnapSDKApp.swift
+//  LukaBluesnapSDK
+//
+//  Created by Jose Moran on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LukaBluesnapSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
